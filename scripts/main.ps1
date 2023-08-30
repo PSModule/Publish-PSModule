0830f4f60f0ebf7aaa857685b0c6683fa22b9656
Marius Storhaug <marstor@hotmail.com>
2023-08-30 20:47:19 +0200

    Update tasks

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -8,5 +8,9 @@ $Task = ($MyInvocation.MyCommand.Name).split('.')[0]
 Write-Verbose "$Task`: Starting..."
 
 Write-Verbose "$Task`: Message: $Message"
+Write-Verbose "$Task`: Bump repo version"
+Write-Verbose "$Task`: Bump module version -> meta"
+Write-Verbose "$Task`: Publish docs to GitHub Pages"
+Write-Verbose "$Task`: Publish module to PowerShell Gallery"
 
 Write-Verbose "$Task`: Stopping..."
