06fa763b8aa5205056766c127faafe939cebe097
Marius Storhaug <marstor@hotmail.com>
2023-08-31 16:39:40 +0200

    test

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -15,3 +15,40 @@ Write-Verbose "$Task`: Update docs path: Update-ModuleMetadata"
 Write-Verbose "$Task`: Publish module to PowerShell Gallery using [$APIKey]"
 
 Write-Verbose "$Task`: Stopping..."
+
+
+function Publish-Module {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string] $ModuleName,
+
+        [Parameter()]
+        [string] $APIKey
+    )
+
+    $SRCPath = Get-Item -Path .\src\ | Select-Object -ExpandProperty FullName
+    $env:PSModulePath += ":$SRCPath"
+    $env:PSModulePath -Split ':'
+
+    $Manifest = Invoke-Expression (Get-Content -Path "src/$ModuleName/$ModuleName.psd1" -Raw)
+    foreach ($Module in $Manifest.RequiredModules) {
+        $InstallParams = @{}
+
+        if ($Module -is [string]) {
+            $InstallParams.Name = $Module
+        } else {
+            $InstallParams.Name = $Module.ModuleName
+            $InstallParams.MinimumVersion = $Module.ModuleVersion
+            $InstallParams.RequiredVersion = $Module.RequiredVersion
+        }
+        $InstallParams.Verbose = $true
+        $InstallParams.Force = $true
+
+        Install-Module @InstallParams
+    }
+
+    .\scripts\Set-ModuleVersion.ps1 -ModuleName $ModuleName -Verbose
+    Publish-Module -Path "src/$ModuleName" -NuGetApiKey $APIKey -Verbose
+
+}
