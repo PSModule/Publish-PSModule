7d06bedb474d33a5612342a02bfd13e3c5febc39
Marius Storhaug <marstor@hotmail.com>
2023-08-31 15:31:26 +0200

    Add publish api key as input

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,16 +1,17 @@
 ﻿[CmdletBinding()]
 param(
-    [Parameter(Mandatory=$true)]
-    [string] $Message
+    [Parameter(Mandatory)]
+    [string] $APIKey
 )
 $Task = ($MyInvocation.MyCommand.Name).split('.')[0]
 
 Write-Verbose "$Task`: Starting..."
 
-Write-Verbose "$Task`: Message: $Message"
+Write-Verbose "$Task`: Generate version"
 Write-Verbose "$Task`: Bump repo version"
-Write-Verbose "$Task`: Bump module version -> meta"
+Write-Verbose "$Task`: Bump module version -> module metadata: Update-ModuleMetadata"
 Write-Verbose "$Task`: Publish docs to GitHub Pages"
-Write-Verbose "$Task`: Publish module to PowerShell Gallery"
+Write-Verbose "$Task`: Update docs path: Update-ModuleMetadata"
+Write-Verbose "$Task`: Publish module to PowerShell Gallery using [$APIKey]"
 
 Write-Verbose "$Task`: Stopping..."
