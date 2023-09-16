54e527a86420d22cb1f12f14ac1c74396e76b777
Marius Storhaug <marstor@hotmail.com>
2023-09-16 23:47:03 +0200

    add versioning and publishing step

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -5,64 +5,103 @@ param(
 )
 $task = New-Object System.Collections.Generic.List[string]
 #region Publish-Module
-$task.Add('Publish-Module')
+$task.Add('Release-Module')
 Write-Output "::group::[$($task -join '] - [')] - Starting..."
-
-
 
 ########################
 # Gather some basic info
 ########################
 
+$outputPath = Get-Item -Path .\outputs\ | Select-Object -ExpandProperty FullName
+$env:PSModulePath += ":$SRCPath"
+$env:PSModulePath -Split ':'
+
+$moduleFolders = Get-ChildItem -Path $outputPath -Directory -Exclude 'docs'
+
+foreach ($module in $moduleFolders) {
+    $moduleName = $module.Name
+    $manifestFilePath = "$module\$moduleName.psd1"
+    $task.Add($moduleName)
+    Write-Output "::group::[$($task -join '] - [')] - Starting..."
+
+    #region Generate-Version
+    $task.Add('Generate-Version')
+    Write-Output "::group::[$($task -join '] - [')]"
+    Write-Verbose "[$($task -join '] - [')] - [] - Generate version"
+    Write-Verbose "[$($task -join '] - [')] - [] - Generate pre-release version if not on main"
+    Write-Verbose "[$($task -join '] - [')] - [] - Create new release with version (prerelease)"
 
 
-#region Generate-Version
-$task.Add('Generate-Version')
-Write-Output "::group::[$($task -join '] - [')]"
-Write-Output "::group::[$($task -join '] - [')] - Do something"
+    [Version]$newVersion = '0.0.0'
 
-Write-Verbose "[$($task -join '] - [')] - [] - Generate version"
-Write-Verbose "[$($task -join '] - [')] - [] - Generate pre-release version if not on main"
-Write-Verbose "[$($task -join '] - [')] - [] - Create new release with version (prerelease)"
-Write-Verbose "[$($task -join '] - [')] - [] - Bump module version -> module metadata: Update-ModuleMetadata"
+    try {
+        $onlineVersion = [Version](Find-Module $moduleName).Version
+    } catch {
+        $onlineVersion = $newVersion
+        Write-Warning "Could not find module online. Using [$($onlineVersion.ToString())]"
+    }
+    Write-Warning "Online: [$($onlineVersion.ToString())]"
+    $manifestVersion = [Version](Test-ModuleManifest $manifestFilePath).Version
+    Write-Warning "Manifest: [$($manifestVersion.ToString())]"
 
-Write-Output "::group::[$($task -join '] - [')] - Done"
-$task.RemoveAt($task.Count - 1)
-#endregion Generate-Version
+    Write-Verbose "branch is: [$env:GITHUB_REF_NAME]"
 
+    if ($manifestVersion.Major -gt $onlineVersion.Major) {
+        $newVersionMajor = $manifestVersion.Major
+        $newVersionMinor = 0
+        $newVersionBuild = 0
+    } else {
+        $newVersionMajor = $onlineVersion.Major
+        if ($manifestVersion.Minor -gt $onlineVersion.Minor) {
+            $newVersionMinor = $manifestVersion.Minor
+            $newVersionBuild = 0
+        } else {
+            $newVersionMinor = $onlineVersion.Minor
+            $newVersionBuild = $onlineVersion.Build + 1
+        }
+    }
+    [Version]$newVersion = [version]::new($newVersionMajor, $newVersionMinor, $newVersionBuild)
+    Write-Warning "newVersion: [$($newVersion.ToString())]"
 
+    if ($env:GITHUB_REF_NAME -ne 'main') {
+        Write-Verbose "prerelease is: [$env:GITHUB_REF_NAME]"
+        Update-ModuleManifest -Path $manifestFilePath -Prerelease $env:GITHUB_REF_NAME -ErrorAction Continue
+    }
 
-#region Publish-Docs
-$task.Add('Publish-Docs')
-Write-Output "::group::[$($task -join '] - [')]"
-Write-Output "::group::[$($task -join '] - [')] - Do something"
+    Write-Verbose "[$($task -join '] - [')] - [] - Bump module version -> module metadata: Update-ModuleMetadata"
+    Update-ModuleManifest -Path $manifestFilePath -ModuleVersion $newVersion -ErrorAction Continue
 
-Write-Verbose "[$($task -join '] - [')] - [] - Publish docs to GitHub Pages"
-Write-Verbose "[$($task -join '] - [')] - [] - Update docs path: Update-ModuleMetadata"
-# What about updateable help? https://learn.microsoft.com/en-us/powershell/scripting/developer/help/supporting-updatable-help?view=powershell-7.3
+    Write-Output "::group::[$($task -join '] - [')] - Done"
+    $task.RemoveAt($task.Count - 1)
+    #endregion Generate-Version
 
-Write-Output "::group::[$($task -join '] - [')] - Done"
-$task.RemoveAt($task.Count - 1)
-#endregion Publish-Docs
+    #region Publish-Docs
+    $task.Add('Publish-Docs')
+    Write-Output "::group::[$($task -join '] - [')]"
+    Write-Output "::group::[$($task -join '] - [')] - Do something"
 
+    Write-Verbose "[$($task -join '] - [')] - [] - Publish docs to GitHub Pages"
+    Write-Verbose "[$($task -join '] - [')] - [] - Update docs path: Update-ModuleMetadata"
+    # What about updateable help? https://learn.microsoft.com/en-us/powershell/scripting/developer/help/supporting-updatable-help?view=powershell-7.3
 
+    Write-Output "::group::[$($task -join '] - [')] - Done"
+    $task.RemoveAt($task.Count - 1)
+    #endregion Publish-Docs
 
-#region Publsih-ToPSGallery
-$task.Add('Publsih-ToPSGallery')
-Write-Output "::group::[$($task -join '] - [')]"
-Write-Output "::group::[$($task -join '] - [')] - Do something"
+    #region Publish-ToPSGallery
+    $task.Add('Publish-ToPSGallery')
+    Write-Output "::group::[$($task -join '] - [')]"
+    Write-Output "::group::[$($task -join '] - [')] - Do something"
 
-Write-Verbose "[$($task -join '] - [')] - [] - Publish module to PowerShell Gallery using [$APIKey]"
-<#
-Publish-Module -Path $Path -NuGetApiKey $APIKey -Verbose -WhatIf
-#>
+    Write-Verbose "[$($task -join '] - [')] - [] - Publish module to PowerShell Gallery using [$APIKey]"
+    Publish-Module -Path "$module" -NuGetApiKey $APIKey -Verbose -WhatIf
 
-Write-Verbose "[$($task -join '] - [')] - [] - Doing something"
-Write-Output "::group::[$($task -join '] - [')] - Done"
-$task.RemoveAt($task.Count - 1)
-#endregion Publsih-ToPSGallery
+    Write-Verbose "[$($task -join '] - [')] - [] - Doing something"
+    Write-Output "::group::[$($task -join '] - [')] - Done"
+    $task.RemoveAt($task.Count - 1)
+    #endregion Publish-ToPSGallery
 
-
+}
 
 $task.RemoveAt($task.Count - 1)
 Write-Output "::group::[$($task -join '] - [')] - Stopping..."
