d3c92fe59c3a07e31568b8371c63026e199fcf90
Marius Storhaug <marstor@hotmail.com>
2024-02-24 03:06:45 +0100

    test

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -8,18 +8,19 @@ Get-ChildItem -Path (Join-Path -Path $env:GITHUB_ACTION_PATH -ChildPath 'scripts
     ForEach-Object { Write-Verbose "[$($_.FullName)]"; . $_.FullName }
 Stop-LogGroup
 
-Write-Verbose "Name:              [$($env:Name)]"
-Write-Verbose "GITHUB_REPOSITORY: [$($env:GITHUB_REPOSITORY)]"
+Write-Verbose "Name:              [$env:Name]"
+Write-Verbose "GITHUB_REPOSITORY: [$env:GITHUB_REPOSITORY]"
+Write-Verbose "GITHUB_WORKSPACE:  [$env:GITHUB_WORKSPACE]"
 
 $name = [string]::IsNullOrEmpty($env:Name) ? $env:GITHUB_REPOSITORY -replace '.+/' : $env:Name
 Write-Verbose "Module name:       [$name]"
 
-$modulePath = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:ModulePath, $name
+$modulePath = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:ModulePath $name
 Write-Verbose "Module path:       [$modulePath]"
 if (-not (Test-Path -Path $modulePath)) {
     throw "Module path [$modulePath] does not exist."
 }
-$docsPath = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:DocsPath, $name
+$docsPath = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:DocsPath $name
 Write-Verbose "Docs path:         [$docsPath]"
 if (-not (Test-Path -Path $docsPath)) {
     throw "Documentation path [$docsPath] does not exist."
