edfa3400435bb53428b8bb3dd4fde4e07a9ade7f
Marius Storhaug <marstor@hotmail.com>
2024-08-19 22:49:19 +0200

    🩹 [Patch]: Use GitHub-Script + linter updates (#34)
    ## Description
    
    - Use GitHub-Script instead of inline, and use LogGroup function instead
    of Start/Stop LogGroup functions.
    - Linter updates - Disable prettier linters.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,29 +1,28 @@
-#REQUIRES -Modules Utilities
-
 [CmdletBinding()]
 param()
 
-Start-LogGroup 'Loading helper scripts'
-Get-ChildItem -Path (Join-Path -Path $env:GITHUB_ACTION_PATH -ChildPath 'scripts' 'helpers') -Filter '*.ps1' -Recurse |
-    ForEach-Object { Write-Verbose "[$($_.FullName)]"; . $_.FullName }
-Stop-LogGroup
-
-Start-LogGroup 'Loading inputs'
-Write-Verbose "Name:              [$env:GITHUB_ACTION_INPUT_Name]"
-Write-Verbose "GITHUB_REPOSITORY: [$env:GITHUB_REPOSITORY]"
-Write-Verbose "GITHUB_WORKSPACE:  [$env:GITHUB_WORKSPACE]"
-
-$name = ($env:GITHUB_ACTION_INPUT_Name | IsNullOrEmpty) ? $env:GITHUB_REPOSITORY_NAME : $env:GITHUB_ACTION_INPUT_Name
-Write-Verbose "Module name:       [$name]"
-Write-Verbose "Module path:       [$env:GITHUB_ACTION_INPUT_ModulePath]"
-Write-Verbose "Doc path:          [$env:GITHUB_ACTION_INPUT_DocsPath]"
-
-$modulePath = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:GITHUB_ACTION_INPUT_ModulePath $name
-Write-Verbose "Module path:       [$modulePath]"
-if (-not (Test-Path -Path $modulePath)) {
-    throw "Module path [$modulePath] does not exist."
+$path = (Join-Path -Path $env:GITHUB_ACTION_PATH -ChildPath 'scripts' 'helpers')
+LogGroup "Loading helper scripts from [$path]" {
+    Get-ChildItem -Path $path -Filter '*.ps1' -Recurse |
+        ForEach-Object { Write-Verbose "[$($_.FullName)]"; . $_.FullName }
 }
-Stop-LogGroup
+
+LogGroup 'Loading inputs' {
+    Write-Verbose "Name:              [$env:GITHUB_ACTION_INPUT_Name]"
+    Write-Verbose "GITHUB_REPOSITORY: [$env:GITHUB_REPOSITORY]"
+    Write-Verbose "GITHUB_WORKSPACE:  [$env:GITHUB_WORKSPACE]"
+
+    $name = ($env:GITHUB_ACTION_INPUT_Name | IsNullOrEmpty) ? $env:GITHUB_REPOSITORY_NAME : $env:GITHUB_ACTION_INPUT_Name
+    Write-Verbose "Module name:       [$name]"
+    Write-Verbose "Module path:       [$env:GITHUB_ACTION_INPUT_ModulePath]"
+    Write-Verbose "Doc path:          [$env:GITHUB_ACTION_INPUT_DocsPath]"
+
+    $modulePath = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:GITHUB_ACTION_INPUT_ModulePath $name
+    Write-Verbose "Module path:       [$modulePath]"
+    if (-not (Test-Path -Path $modulePath)) {
+        throw "Module path [$modulePath] does not exist."
+    }
+}
 
 $params = @{
     Name       = $name
