43de48d6b93cbd1ffb015bd9eb2f32abec64df04
Marius Storhaug <marstor@hotmail.com>
2025-02-28 20:21:54 +0100

    🩹 [Fix]: Remove GitHub workspace path from WorkingDirectory input for consistency in module publishing

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -10,33 +10,16 @@ LogGroup "Loading helper scripts from [$path]" {
 }
 
 LogGroup 'Loading inputs' {
-
-    [pscustomobject]@{
-        Name              = $env:PSMODULE_PUBLISH_PSMODULE_INPUT_Name
-        WorkingDirectory  = $env:PSMODULE_PUBLISH_PSMODULE_INPUT_WorkingDirectory
-        ModulePath        = $env:PSMODULE_PUBLISH_PSMODULE_INPUT_ModulePath
-        DocsPath          = $env:PSMODULE_PUBLISH_PSMODULE_INPUT_DocsPath
-        APIKey            = $env:PSMODULE_PUBLISH_PSMODULE_INPUT_APIKey
-        GITHUB_REPOSITORY = $env:GITHUB_REPOSITORY
-    } | Format-List
-
-
     $name = if ([string]::IsNullOrEmpty($env:PSMODULE_PUBLISH_PSMODULE_INPUT_Name)) {
         $env:GITHUB_REPOSITORY_NAME
     } else {
         $env:PSMODULE_PUBLISH_PSMODULE_INPUT_Name
     }
-    $modulePath = Resolve-Path -Path "$env:PSMODULE_PUBLISH_PSMODULE_INPUT_WorkingDirectory/$env:PSMODULE_PUBLISH_PSMODULE_INPUT_ModulePath/$name" |
-        Select-Object -ExpandProperty Path
-    Write-Verbose "Resolved path:     [$modulePath]"
-
+    $modulePath = Resolve-Path -Path "$env:PSMODULE_PUBLISH_PSMODULE_INPUT_ModulePath/$name" | Select-Object -ExpandProperty Path
     [pscustomobject]@{
-        Name              = $name
-        WorkingDirectory  = $env:PSMODULE_PUBLISH_PSMODULE_INPUT_WorkingDirectory
-        ModulePath        = $modulePath
-        DocsPath          = $env:PSMODULE_PUBLISH_PSMODULE_INPUT_DocsPath
-        APIKey            = $env:PSMODULE_PUBLISH_PSMODULE_INPUT_APIKey
-        GITHUB_REPOSITORY = $env:GITHUB_REPOSITORY
+        Name       = $name
+        ModulePath = $modulePath
+        APIKey     = $env:PSMODULE_PUBLISH_PSMODULE_INPUT_APIKey
     } | Format-List
 
 }
